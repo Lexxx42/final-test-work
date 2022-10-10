b8294f9019a77ed33222ba394941ce2b01bb3779
Alexander Konukhov <jesusmisha@gmail.com>
2022-10-10 22:08:27 +0300

    main program

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Write a program that from the existing array of strings 
+// forms an array of strings whose length is less than or equal to 3 characters.
+
+void PrintArray(string[] array) // Print array.
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+string[] FillArrayOutput(string[] array) // Fills array output.
+{
+    string[] arrayOut = new string[array.Length];
+    int indexOut = 0;
+    foreach (var item in array)
+    {
+
+        if (item.Length <= 3)
+        {
+            arrayOut[indexOut] = item;
+            indexOut++;
+        }
+    }
+    return arrayOut;
+}
+
+
+
+string[] arrayInput = { "Hello", "2", "world", ":-)" };
+string[] arrayOutput = FillArrayOutput(arrayInput);
+PrintArray(arrayOutput);
